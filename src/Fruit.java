e2d2e356e385c961512e4d0e4da2166d8515dc87
yokozhem <kozhemyakova.elena@gmail.com>
2021-09-28 13:57:56 +0700

    my third homework

---
+++ src/Fruit.java
@@ -0,0 +1,11 @@
+abstract class Fruit {
+    private float weight;
+
+    Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
